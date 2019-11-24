eb55ef5f47fc3173fbf1dc84a754952b72ff8788
mstrYoda <freelancemuhendis@gmail.com>
2019-11-24 23:28:19 +0300

    add comment line to simple channel deadlock

--- concurrency/simple-channel-deadlock.go
+++ concurrency/simple-channel-deadlock.go
@@ -12,8 +12,13 @@ func listen(ch chan int) {
 func main() {
 
 	ch := make(chan int)
+
 	go listen(ch)
 
+	// here go scheduler blocks main goroutine to schedule another goroutine to read from channel
+	// when it first sends data to channel, listen() goroutine gets value from channel end it ends
+	// when we send another data, it will get "fatal error: all goroutines are asleep - deadlock!" error
+	// because there is not any other listener for the channel
 	ch <- 1
 	ch <- 2
 
